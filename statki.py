49f9d8956860d3098547cc1089f26d1852b0ca51
Magicninja7 <j.t.ptaszek@gmail.com>
2024-11-26 19:22:13 +0100

    create boards

--- statki.py
+++ statki.py
@@ -10,7 +10,7 @@ pla_possible_ships = []
 move = 'player'
 
 
-
+#bot chooses where to shoot, based on where a close X is
 def axis(xory):
     if xory == 'x':
         x, y = pla_possible_ships[0]
@@ -56,35 +56,147 @@ def bot_check():
     return True
 
 
+#player creates board
+def pla_create_board():
+    pla_set = []
+    for i in range(1, 6):
+        ship, direc, leng = input(f'ship {i} ({i})').split('|')
+        x, y = ship.split(',')
+        pla_set.append((x, y, direc, leng))
+
+    for ship in pla_set:
+        x, y, direc, leng = ship
+        x, y = int(x), int(y)
+        leng = int(leng)
+        
+        for i in range(leng):
+            if direc == 'top':
+                pla_board[x - i][y] = 1
+            elif direc == 'right':
+                pla_board[x][y + i] = 1
+            elif direc == 'bottom':
+                pla_board[x + i][y] = 1
+            elif direc == 'left':
+                pla_board[x][y - i] = 1
+
+#bot creates board
+def bot_create_board():
+    used_cords = []
+    length = 5
+    def check_board():
+        global length
+        correct = False
+        while correct == False:
+            x = random.randint(0, 9)
+            y = random.randint(0, 9)
+            if check_inside(x, y):
+                correct = True
+        length -= 1
+
+    def check_inside(x, y):
+        global length
+        end_x = -1
+        end_y = -1
+        directions = ['r', 'b', 'l', 't']
+        for _ in range(4):
+            if directions == []:
+                return False
+            direc = random.choice(directions)
+            directions.remove(direc)
+            match direc:
+                case 'r':
+                    end_y = y + length
+                    end_x = x
+                case 'l':
+                    end_y = y - length
+                    end_x = x
+                case 't':
+                    end_y = y
+                    end_x = x - length
+                case 'b':
+                    end_y = y
+                    end_x = x + length
+            if 0 <= end_x < 10 and 0 <= end_y < 10:
+                if other_ships(x, y, length, direc):
+                    for c in range(length): ### add cused cords and update board
+                        match direc:
+                            case 'r':
+                                end_y = y + c
+                                end_x = x
+                            case 'l':
+                                end_y = y - c
+                                end_x = x
+                            case 't':
+                                end_y = y
+                                end_x = x - c
+                            case 'b':
+                                end_y = y
+                                end_x = x + c
+                        used_cords.append((end_x, end_y))
+                        bot_board[end_x][end_y] = 1               
+                    return True
+                continue
+        return False
+        
+    def other_ships(x, y, length, direc):
+        for i in range(length):
+            match direc:
+                case 'r':
+                    curr_y = y + i
+                    curr_x = x
+                case 'l':
+                    curr_y = y - i
+                    curr_x = x
+                case 't':
+                    curr_y = y
+                    curr_x = x - i
+                case 'b':
+                    curr_y = y
+                    curr_x = x + i
+            if (curr_x, curr_y) in used_cords:
+                return False
+        return True
+
+    for d in range(5):
+        check_board()
 
 
+#prints players board
+def pla_print_board():
+    print('Player board:')
+    print('Bot board:')
+    for i in range(10):
+        print("|", end="")
+        for j in range(10):
+            if pla_board[i][j] == 'X':
+                print('X', end="|")
+            elif pla_board[i][j] == 'O':
+                print('O', end="|")
+            elif pla_board[i][j] == 1:
+                print('1', end="|")
+            else:
+                print('.', end="|")
+        print()
+        if i < 9:
+            print("-" * 21)
 
-pla_set = []
-for i in range(1, 6):
-    ship, direc, leng = input(f'ship {i} ({i})').split('|')
-    x, y = ship.split(',')
-    pla_set.append((x, y, direc, leng))
-
-for ship in pla_set:
-    x, y, direc, leng = ship
-    x, y = int(x), int(y)
-    leng = int(leng)
-    
-    for i in range(leng):
-        if direc == 'top':
-            pla_board[y - i][x] = 1
-        elif direc == 'right':
-            pla_board[y][x + i] = 1
-        elif direc == 'bottom':
-            pla_board[y + i][x] = 1
-        elif direc == 'left':
-            pla_board[y][x - i] = 1
-
-# Print the matrix
-for row in pla_board:
-    print(' '.join(map(str, row)))
-
-
+#prints bots board
+def bot_print_board():
+    print('Bot board:')
+    for i in range(10):
+        print("|", end="")
+        for j in range(10):
+            if bot_board[i][j] == 'X':
+                print('X', end="|")
+            elif bot_board[i][j] == 'O':
+                print('O', end="|")
+            elif bot_board[i][j] == 1:
+                print('1', end="|")
+            else:
+                print('.', end="|")
+        print()
+        if i < 9:
+            print("-" * 21)
 
 
 
@@ -94,6 +206,8 @@ for row in pla_board:
 
 
 def main():
+    pla_create_board()
+    bot_create_board()
     
 
     while True:
@@ -117,10 +231,10 @@ def main():
                 print('Miss!')
 
         while move == 'bot':
-            #bot move###
+            #bot move
             if pla_possible_ships == []:
-                x = random.randint(0, 9)
-                y = random.randint(0, 9)
+                x = random.randint(0, 10)
+                y = random.randint(0, 10)
                 if pla_board[x][y] == 1:
                     pla_board[x][y] = 'X'
                 else:
@@ -146,40 +260,10 @@ def main():
                     axis('y')
                 else:
                     axis('y')
+        pla_print_board()
+        bot_print_board()
 
-            print('Player board:')
-
-            print("|", end='')
-            for i in range(10):
-                for j in range(10):
-                    if pla_board[i][j] == 'X':
-                        print('X', end='')
-                    elif pla_board[i][j] == 'O':
-                        print('O', end='')
-                    elif pla_board[i][j] == 1:
-                        print('1', end='')
-                    else:
-                        print('.', end='')
-                    print("|")
-                for _ in range(10):
-                    print('-', end='')
-
-            print('Bot board:')
-            ###
-            print("|", end='')
-            for i in range(10):
-                for j in range(10):
-                    if bot_board[i][j] == 'X':
-                        print('X', end='')
-                    elif bot_board[i][j] == 'O':
-                        print('O', end='')
-                    elif bot_board[i][j] == 1:
-                        print('1', end='')
-                    else:
-                        print('.', end='')
-                    print("|")
-                for _ in range(10):
-                    print('-', end='')
+            
 
 
 
